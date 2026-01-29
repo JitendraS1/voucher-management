369c969c7d076babd250de59b393769a2b820c47
User <user@example.com>
2026-01-29 19:18:41 +0530

    Remove duplicate Export to Excel button

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -644,15 +644,6 @@ function SubmittedVouchersPage({
             >
               New Voucher
             </button>
-            <button
-              onClick={onExportExcel}
-              className="bg-green-600 text-white px-5 py-2 rounded-lg shadow-md hover:bg-green-700 transition duration-300 ease-in-out transform hover:scale-105 flex items-center gap-2"
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
-              </svg>
-              Export to Excel
-            </button>
           </div>
         </div>
 
